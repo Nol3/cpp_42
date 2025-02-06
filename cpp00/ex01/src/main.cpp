3122a5fe04e96824dc31994c277030dda868dc2b
Nol3 <alejandrocp_952@hotmail.com>
2025-02-06 20:18:57 +0100

    fix of phonebook (EOF)

--- cpp00/ex01/src/main.cpp
+++ cpp00/ex01/src/main.cpp
@@ -1,53 +1,100 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alcarden <alcarden@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/06 15:54:39 by alcarden          #+#    #+#             */
+/*   Updated: 2025/02/06 18:39:52 by alcarden         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "contact.hpp"
 #include "phonebook.hpp"
+#include <cstdlib>
+#include <string.h>
 
-#include <iostream>
+std::string getInputSafely(const std::string &prompt)
+{
+	std::string input;
+	std::cout << prompt;
+	if (!std::getline(std::cin, input))
+	{
+		std::cout << "\nEOF detected, exiting program." << std::endl;
+		exit(0);
+	}
+	return (input);
+}
 
-int main() {
-    PhoneBook phoneBook;
-    std::string command;
+int	main(void)
+{
+	PhoneBook phoneBook;
+	std::string command;
 
-    while (true) {
-        std::cout << "Enter command (ADD, SEARCH, EXIT): ";
-        std::getline(std::cin, command);
+	while (true)
+	{
+		std::cout << "Enter command (ADD, SEARCH, EXIT): ";
+		if (!std::getline(std::cin, command))
+		{
+			std::cout << "\nEOF detected, exiting program." << std::endl;
+			break ;
+		}
 
-        if (command == "ADD") {
-            Contact contact;
-            std::string input;
+		if (command == "ADD")
+		{
+			Contact contact;
 
-            std::cout << "Enter first name: ";
-            std::getline(std::cin, input);
-            contact.setFirstName(input);
+			contact.setFirstName(getInputSafely("Enter first name: "));
+			contact.setLastName(getInputSafely("Enter last name: "));
+			contact.setNickname(getInputSafely("Enter nickname: "));
+			contact.setPhoneNumber(getInputSafely("Enter phone number: "));
+			contact.setDarkestSecret(getInputSafely("Enter darkest secret: "));
 
-            std::cout << "Enter last name: ";
-            std::getline(std::cin, input);
-            contact.setLastName(input);
+			phoneBook.addContact(contact);
+		}
+		else if (command == "SEARCH")
+		{
+			phoneBook.displayContacts();
+			std::cout << "Enter index to view details: ";
+			std::string indexStr;
+			if (!std::getline(std::cin, indexStr))
+			{
+				std::cout << "\nEOF detected, exiting program." << std::endl;
+				break ;
+			}
 
-            std::cout << "Enter nickname: ";
-            std::getline(std::cin, input);
-            contact.setNickname(input);
+			int index = 0;
+			bool valid = true;
 
-            std::cout << "Enter phone number: ";
-            std::getline(std::cin, input);
-            contact.setPhoneNumber(input);
+			for (size_t i = 0; i < indexStr.length(); i++)
+			{
+				if (!isdigit(indexStr[i]))
+				{
+					valid = false;
+					break ;
+				}
+			}
 
-            std::cout << "Enter darkest secret: ";
-            std::getline(std::cin, input);
-            contact.setDarkestSecret(input);
+			if (valid && indexStr.length() > 0)
+			{
+				index = atoi(indexStr.c_str());
+				phoneBook.displayContactDetails(index);
+			}
+			else
+			{
+				std::cout << "Invalid index" << std::endl;
+			}
+		}
+		else if (command == "EXIT")
+		{
+			break ;
+		}
+		else
+		{
+			std::cout << "Invalid command" << std::endl;
+		}
+	}
 
-            phoneBook.addContact(contact);
-        } else if (command == "SEARCH") {
-            phoneBook.displayContacts();
-            std::cout << "Enter index to view details: ";
-            int index;
-            std::cin >> index;
-            std::cin.ignore();
-            phoneBook.displayContactDetails(index);
-        } else if (command == "EXIT") {
-            break;
-        } else {
-            std::cout << "Invalid command" << std::endl;
-        }
-    }
-
-    return 0;
+	return (0);
 }
--- cpp00/ex01/src/phonebook.cpp
+++ cpp00/ex01/src/phonebook.cpp
@@ -6,7 +6,7 @@
 /*   By: alcarden <alcarden@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/30 16:48:36 by alcarden          #+#    #+#             */
-/*   Updated: 2024/11/07 13:22:25 by alcarden         ###   ########.fr       */
+/*   Updated: 2025/02/06 18:21:00 by alcarden         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,43 +53,3 @@ void PhoneBook::displayContactDetails(int index) const {
     std::cout << "Phone Number: " << contacts[index].getPhoneNumber() << std::endl;
     std::cout << "Darkest Secret: " << contacts[index].getDarkestSecret() << std::endl;
 }
-
-// int	ft_strncmp(const char *s1, const char *s2, size_t n)
-// {
-// 	unsigned int	i;
-
-// 	i = 0;
-// 	while ((s1[i] != '\0' || s2[i] != '\0') && i < n)
-// 	{
-// 		if ((unsigned char)s1[i] > (unsigned char)s2[i])
-// 		{
-// 			return (1);
-// 		}
-// 		if ((unsigned char)s1[i] < (unsigned char)s2[i])
-// 		{
-// 			return (-1);
-// 		}
-// 		i++;
-// 	}
-// 	return (0);
-// }
-
-// int main(int argc, char const *argv[])
-// {
-// 	char buff[512];
-
-// 	Phonebook phone;
-// 	while (exit)
-// 	{
-// 		std::cin >> buff;
-// 		if ()
-// 		{
-// 			/* code */
-// 		}
-// 		ft_strncmp(buff, "ADD", 3);
-// 		ft_strncmp(buff, "EXIT", 4);
-// 		ft_strncmp(buff, "SEARCH", 6);
-
-// 	}
-// 	return 0;
-// }
